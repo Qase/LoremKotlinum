9809c2d5dbdcd5d956cbd55591f49c77f8b6729f
mka <miroslav.kaspar@quanti.cz>
2017-12-01 17:01:00 +0100

    fun paragraps() update - middleSeparator should be Separator.NONE MainActivity (demo) updates

--- app/src/main/java/cz/quanti/qase/loremkotlinum/MainActivity.kt
+++ app/src/main/java/cz/quanti/qase/loremkotlinum/MainActivity.kt
@@ -11,8 +11,8 @@ class MainActivity : AppCompatActivity() {
 
     val sb = StringBuilder();
     
-    val begSign = "»"
-    val endSign = "«"
+    val begMark = ""//"»"
+    val endMark = ""//"«"
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +22,20 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+    }
+
+    fun initHolder() {
+        sb.delete(0, sb.length)
+        //sb.append("Characters " + begMark + " and " + endMark + " are not part of the " + getString(R.string.app_name) +  " generated text.\n\n")
+    }
+
+    fun store2Holder(string: String) {
+        sb.append(string + "\n\n")
+    }
+
+    fun dispayHolder() {
+        scroll_view.scrollTo(0, 0)
+        text_view.setText(sb.toString())
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -38,71 +52,136 @@ class MainActivity : AppCompatActivity() {
             R.id.action_word -> {
                 initHolder()
                 for (i in 1..16) {
-                    store2Holder( "♦ word():\n"+ begSign + Lorem.word() + endSign)
+                    store2Holder( "♦ Lorem.word():\n"+ begMark + Lorem.word() + endMark)
                 }
 
                 dispayHolder()
                 return true
             }
+
             R.id.action_words -> {
                 initHolder()
                 for (i in 1..4) {
-                    store2Holder( "♦ words():\n"+ begSign + Lorem.words() + endSign)
+                    store2Holder( "♦ Lorem.words():\n"+ begMark + Lorem.words() + endMark)
                 }
 
-                for (i in 1..16) {
-                    store2Holder( "♦ words(" + i + "):\n"+ begSign + Lorem.words(i) + endSign)
+                for (i in 1..Lorem.maxWords) {
+                    store2Holder( "♦ Lorem.words(" + i + "):\n"+ begMark + Lorem.words(i) + endMark)
                 }
 
                 dispayHolder()
                 return true
             }
+
             R.id.action_sentence -> {
                 initHolder()
                 for (i in 1..4) {
-                    store2Holder( "♦ sentnece():\n"+ begSign + Lorem.sentence() + endSign)
+                    store2Holder( "♦ Lorem.sentnece():\n"+ begMark + Lorem.sentence() + endMark)
                 }
 
-                for (i in 1..16) {
-                    store2Holder( "♦ sentnece(" + i +"):\n"+ begSign + Lorem.sentence(i) + endSign)
+                for (i in 1..Lorem.maxWords) {
+                    store2Holder( "♦ Lorem.sentnece(" + i +"):\n"+ begMark + Lorem.sentence(i) + endMark)
                 }
 
                 dispayHolder()
                 return true
             }
+
             R.id.action_sentences -> {
                 initHolder()
                 for (i in 1..4) {
-                    store2Holder( "♦ sentences():\n"+ begSign + Lorem.sentences() + endSign)
+                    store2Holder( "♦ Lorem.sentences():\n"+ begMark + Lorem.sentences() + endMark)
                 }
 
-                for (i in 1..16) {
-                    store2Holder( "♦ sentences(" + i + "):\n"+ begSign + Lorem.sentences(i) + endSign)
+                for (i in 1..Lorem.maxSentences) {
+                    store2Holder( "♦ Lorem.sentences(" + i + "):\n"+ begMark + Lorem.sentences(i) + endMark)
                 }
 
                 dispayHolder()
                 return true
             }
+
             R.id.action_paragraph -> {
                 initHolder()
                 for (i in 1..4) {
-                    store2Holder( "♦ paragraph():\n"+ begSign + Lorem.paragraph() + endSign)
+                    store2Holder( "♦ Lorem.paragraph():\n"+ begMark + Lorem.paragraph() + endMark)
                 }
-                for (i in 1..16) {
-                    store2Holder( "♦ paragraph(" + i + "):\n"+ begSign + Lorem.paragraph(i) + endSign)
+                for (i in 1..Lorem.maxSentences) {
+                    store2Holder( "♦ Lorem.paragraph(" + i + "):\n"+ begMark + Lorem.paragraph(i) + endMark)
                 }
 
                 dispayHolder()
                 return true
             }
+
             R.id.action_paragraphs -> {
                 initHolder()
                 for (i in 1..4) {
-                    store2Holder( "♦ paragraphs():\n"+ begSign + Lorem.paragraphs() + endSign)
+                    store2Holder( "♦ Lorem.paragraphs():\n"+ begMark + Lorem.paragraphs() + endMark)
                 }
 
+                for (i in 1..Lorem.maxParagraps) {
+                    store2Holder( "♦ Lorem.paragraphs(" + i + "):\n"+ begMark + Lorem.paragraphs(i) + endMark)
+                }
+
+                dispayHolder()
+                return true
+            }
+
+            R.id.action_first_name -> {
+                initHolder()
                 for (i in 1..16) {
-                    store2Holder( "♦ paragraphs(" + i + "):\n"+ begSign + Lorem.paragraphs(i) + endSign)
+                    store2Holder( "♦ Lorem.firstName():\n"+ begMark + Lorem.firstName() + endMark)
+                }
+
+                dispayHolder()
+                return true
+            }
+
+            R.id.action_last_name -> {
+                initHolder()
+                for (i in 1..16) {
+                    store2Holder( "♦ Lorem.lastName():\n"+ begMark + Lorem.lastName() + endMark)
+                }
+
+                dispayHolder()
+                return true
+            }
+
+            R.id.action_name -> {
+                initHolder()
+                for (i in 1..16) {
+                    store2Holder( "♦ Lorem.name():\n"+ begMark + Lorem.name() + endMark)
+                }
+
+                dispayHolder()
+                return true
+            }
+
+            R.id.action_domain -> {
+                initHolder()
+                for (i in 1..16) {
+                    store2Holder( "♦ Lorem.domain():\n"+ begMark + Lorem.domain() + endMark)
+                }
+
+                dispayHolder()
+                return true
+            }
+
+            R.id.action_email -> {
+                initHolder()
+                for (i in 1..16) {
+                    store2Holder( "♦ Lorem.email():\n"+ begMark + Lorem.email() + endMark)
+                }
+
+                dispayHolder()
+                return true
+            }
+
+            R.id.action_tweet -> {
+                initHolder()
+                for (i in 1..16) {
+                    store2Holder( "♦ Lorem.tweet():\n"+ begMark + Lorem.tweet() + endMark)
                 }
 
                 dispayHolder()
@@ -111,19 +190,5 @@ class MainActivity : AppCompatActivity() {
         }
 
             return super.onOptionsItemSelected(item)
-    }
-
-    fun initHolder() {
-        sb.delete(0, sb.length)
-        sb.append("Characters ♦, " + begSign + " and " + endSign + " are not part of the " + getString(R.string.app_name) +  " generated text.\n\n")
-    }
-    
-    fun store2Holder(string: String) {
-        sb.append(string + "\n\n")
-    }
-
-    fun dispayHolder() {
-        scroll_view.scrollTo(0, 0)
-        text_view.setText(sb.toString())
     }
 }
